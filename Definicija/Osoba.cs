93f8c961ee0809d0249d7e97f161bca5b806e7b8
josipjuretic <juretic.josip@gmail.com>
2015-12-05 19:57:26 +0100

    Verzija 1.1.2

--- Definicija/Osoba.cs
+++ Definicija/Osoba.cs
@@ -10,20 +10,49 @@ namespace Vsite.CSharp
             Prezime = prezime;
         }
 
-        // TODO: Javno dostupno polje Prezime nadomjestiti svojstvom (property) koje se izvan klase može samo čitati, a mijenjati se može samo iz klase
+        // Javno dostupno polje Prezime nadomjestiti svojstvom (property) koje se izvan klase može samo čitati, a mijenjati se može samo iz klase
 
-        public string Prezime;
+        public string Prezime
+    {
+        get;
+        private set;
+    }
 
-
-        // TODO: Javno dostupno polje Ime nadomjestiti svojstvom (property) koje se izvan klase može čitati i zadavati, ali prilikom zadavanja treba onemogućiti
+        // Javno dostupno polje Ime nadomjestiti svojstvom (property) koje se izvan klase može čitati i zadavati, ali prilikom zadavanja treba onemogućiti
         // zadavanje praznim znakovnim nizom
 
-        public string Ime;
+        private string m_ime;
+        public string Ime
+    {
+        get;
+        {
+            return m_ime;
+        }
+        set 
+        { 
+            if (value == "")
+            return;
+            m_ime = value;
+        }
+    }
 
-        // TODO: Javno dostupno polje DatumRođenja nadomjestiti svojstvom (property) koje se izvan klase može čitati i zadavati, ali za slučaj zadavanja
+        // Javno dostupno polje DatumRođenja nadomjestiti svojstvom (property) koje se izvan klase može čitati i zadavati, ali za slučaj zadavanja
         // datuma većeg od trenutnog treba baciti iznimku tipa ArgumentOutOfRangeException. U pozivajućem kodu staviti odogovarajući kod za hvatanje
         // iznimke koji će u slučaju iznimke ispisati odgovarajuću poruku.
+        private DateTime m_datumRođenja;
 
         public DateTime DatumRođenja;
+    {
+        get
+        {
+        return m_datumRođenja;
+        }
+        set
+        {
+        if (ValueType >= DateTime.Now)
+        throw new ArgumentOutOfRangeException();
+        m_datumRođenja = value;
+        }
     }
+    
 }
